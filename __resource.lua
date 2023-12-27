8105c715368d81a29f0c073e6c5bfe3b78a6415a
6ur4cc <144011794+6ur4cc@users.noreply.github.com>
2023-12-27 18:40:17 +0300

    Create __resource.lua

---
+++ __resource.lua
@@ -0,0 +1,13 @@
+--//** created by 6ur4cc with love <3 **\\--
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author '6UR4CC <hi@6ur4cc.nl>'
+description 'QBCore Disable Radio'
+version '1.0'
+
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+client_script {
+'client.lua',
+}
